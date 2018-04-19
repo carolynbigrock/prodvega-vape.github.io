704af24b0830825e68b6637904752087af5ccf06
Sevanti Nag <sevanti@westagilelabs.com>
2018-04-19 17:53:33 +0530

    Finalised UI for Page

--- js/script.js
+++ js/script.js
@@ -1,10 +1,1 @@
-$(document).ready(function(){
-    var isshow = localStorage.getItem('ageCleared');
-    if (isshow== null) {
-        localStorage.setItem('ageCleared', 1);
-        $('#age_restriction_modal').modal('show'); 
-    }
-    $('.age-accept-btn').click(function(){
-        $('#age_restriction_modal').modal('hide'); 
-    })
-});
+$(document).ready(function(){null==localStorage.getItem("ageCleared")&&(localStorage.setItem("ageCleared",1),$("#age_restriction_modal").modal("show")),$(".age-accept-btn").click(function(){$("#age_restriction_modal").modal("hide")})});
